9584b2c48406c42af056e119c288e3b286c02278
erojas91 <esteban.j.rojas50@gmail.com>
2019-01-24 09:19:48 -0300

    prueba commit, modificacion servicio tabgral

--- GestionComercial/src/app/servicios/tabgral.service.ts
+++ GestionComercial/src/app/servicios/tabgral.service.ts
@@ -2,6 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+const httpOptions = {
+  headers: new HttpHeaders(
+    { 'Content-Type': 'application/json',
+      'Authorization':  localStorage.getItem('tk_acceso')
+    }
+  )
+};
+
+const apiUrl = "http://192.168.1.44:3000";
 
 @Injectable({
   providedIn: 'root'
