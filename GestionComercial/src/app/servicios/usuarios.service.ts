ce44937a07df7f0eb464fe770a71c4a43e92d66b
erojas91 <esteban.j.rojas50@gmail.com>
2019-03-08 14:22:19 -0300

    Busqueda de usuario por diferentes parametros. Funcionando.

--- GestionComercial/src/app/servicios/usuarios.service.ts
+++ GestionComercial/src/app/servicios/usuarios.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 const httpOptions = {
   headers: new HttpHeaders(
-    { 'Content-Type': 'application/json',
-      'Authorization':  localStorage.getItem('roma_acceso')
+    {
+      'Content-Type': 'application/json',
+      'Authorization': localStorage.getItem('roma_acceso')
     }
   )
 };
@@ -17,8 +19,18 @@ export class UsuariosService {
 
   constructor(private http: HttpClient) { }
 
-  getDatosUsuario(usuario){
-    return this.http.get(environment.apiEndpoint+'/getDatosUsuario/'+usuario, httpOptions);
+  getDatosUsuario(usuario) {
+    return this.http.get(environment.apiEndpoint + '/getDatosUsuario/' + usuario, httpOptions);
   }
+
+  getUsuariosBusqueda(texto_busqueda): Observable<any> {
+    return this.http.get(environment.apiEndpoint + '/getUsuariosBusqueda/' + texto_busqueda, httpOptions);
+  }
+
+  getUsuariosTodos(): Observable<any> {
+    return this.http.get(environment.apiEndpoint + '/getUsuariosTodos/', httpOptions);
+  }
+
+
 
 }
