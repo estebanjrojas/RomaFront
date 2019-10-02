25c93a35da10c43b339e5c192d5aff8009f3d3b9
erojas91 <esteban.j.rojas50@gmail.com>
2019-10-02 10:42:47 -0300

    Paginación de ABM Clientes

--- GestionComercial/src/app/componentes/adm-clientes/buscar-clientes/buscar-clientes.component.ts
+++ GestionComercial/src/app/componentes/adm-clientes/buscar-clientes/buscar-clientes.component.ts
@@ -10,42 +10,91 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class BuscarClientesComponent implements OnInit {
 
-  buscarClientesForm: FormGroup;
   cast: any;
+  buscar: string = "";
+  cantidad_paginas: number = 0;
+  pagina_actual: number = 1;
+  buscar_nombre: boolean = true;
+  buscar_apellido: boolean = true;
+  buscar_dni: boolean = true;
+  buscar_fecha_nac: boolean = true;
+  valor_boton_1: number = 1;
+  valor_boton_2: number = 2;
+  valor_boton_3: number = 3;
 
 
-  constructor(private SrvClientes: ClientesService,
+  constructor(
+    private SrvClientes: ClientesService,
     private SrvToastr: ToastrService,
-    private formBuilder: FormBuilder) {
-    this.buscarClientesForm = this.formBuilder.group({
-      nombre_usuario: [
-        '', Validators.compose([
-
-        ])
-      ],
-      txtBuscar: []
-    });
-  }
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
     this.buscarClientes();
   }
 
-
   buscarClientes() {
-    let busqueda = this.buscarClientesForm.controls.txtBuscar.value;
-    if (busqueda == undefined || busqueda == '') {
-      this.SrvClientes.getClientesTodos().subscribe(respuesta => {
-        this.cast = respuesta;
-        console.log({ "SrvCategorias.getClientesTodos": this.cast });
-      });
+    let buscar_por_nombre = 1;
+    let buscar_por_apellido = 1;
+    let buscar_por_dni = 1;
+    let buscar_por_fecha_nac = 1;
+    if (this.buscar_nombre) { buscar_por_nombre = 1; } else { buscar_por_nombre = 0; }
+    if (this.buscar_apellido) { buscar_por_apellido = 1; } else { buscar_por_apellido = 0; }
+    if (this.buscar_dni) { buscar_por_dni = 1; } else { buscar_por_dni = 0; }
+    if (this.buscar_fecha_nac) { buscar_por_fecha_nac = 1; } else { buscar_por_fecha_nac = 0; }
+    this.SrvClientes.getCantidadPaginasClientes(buscar_por_nombre, buscar_por_apellido, buscar_por_dni, buscar_por_fecha_nac, this.buscar).subscribe(respuesta => {
+      let cast: any = respuesta.regCantidadPaginas.cantidad_paginas;
+      this.cantidad_paginas = cast;
+      this.pagina_actual = 1;
+      this.setCantidadPaginas();
+    }, error => {
+      console.log(JSON.stringify(error));
+    });
+  }
+
+  /** trae el detalle en base a la posicion y busqueda */
+  getItems() {
+    let buscar_por_nombre = 1;
+    let buscar_por_apellido = 1;
+    let buscar_por_dni = 1;
+    let buscar_por_fecha_nac = 1;
+    if (this.buscar_nombre) { buscar_por_nombre = 1; } else { buscar_por_nombre = 0; }
+    if (this.buscar_apellido) { buscar_por_apellido = 1; } else { buscar_por_apellido = 0; }
+    if (this.buscar_dni) { buscar_por_dni = 1; } else { buscar_por_dni = 0; }
+    if (this.buscar_fecha_nac) { buscar_por_fecha_nac = 1; } else { buscar_por_fecha_nac = 0; }
+    this.SrvClientes.getClientes(this.pagina_actual, this.cantidad_paginas, buscar_por_nombre, buscar_por_apellido, buscar_por_dni, buscar_por_fecha_nac, this.buscar).subscribe(respuesta => {
+      this.cast = respuesta;
+    }, error => {
+      console.log(JSON.stringify(error));
+    });
+  }
+  /** posiciona el indice en el boton seleccionado */
+  setPaginaByComponenteId(valor) {
+    this.pagina_actual = valor;
+    this.setCantidadPaginas();
+  }
+  /** habilita la cantidad de botones indices en base a la cant de paginas */
+  setCantidadPaginas() {
+    if (this.pagina_actual <= 0) {
+      this.pagina_actual = 1;
+    } if (this.pagina_actual >= this.cantidad_paginas) {
+      this.pagina_actual = this.cantidad_paginas;
     }
-    else {
-      this.SrvClientes.getClientesBusqueda(busqueda).subscribe(respuesta => {
-        this.cast = respuesta;
-        console.log({ "SrvCategorias.getClientesBusqueda": this.cast });
-      });
+    this.paginarEn(this.pagina_actual);
+    this.getItems();
+  }
+  /** enumera los botones en base a la pagina actual */
+  paginarEn(paginarEnNumero) {
+    if (paginarEnNumero <= 2) {
+      paginarEnNumero = 2;
+    } else {
+      if (paginarEnNumero == this.cantidad_paginas) {
+        paginarEnNumero = (this.cantidad_paginas - 1);
+      }
     }
+    this.valor_boton_1 = paginarEnNumero - 1;
+    this.valor_boton_2 = paginarEnNumero;
+    this.valor_boton_3 = paginarEnNumero + 1;
   }
 
 }
--- GestionComercial/src/app/servicios/clientes.service.ts
+++ GestionComercial/src/app/servicios/clientes.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
-import { Clientes} from '../modelos/Clientes';
+import { Clientes } from '../modelos/Clientes';
+
+
+const httpOptions = {
+  headers: new HttpHeaders(
+    {
+      'Content-Type': 'application/json',
+      'Authorization': localStorage.getItem('roma_acceso')
+    }
+  )
+};
 
 
 
@@ -23,62 +33,46 @@ export class ClientesService {
     return this.miCliente;
   }
 
+  //---------------------------GET's---------------------------//
+
   getClientesBusqueda(texto_busqueda): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders(
-        { 'Content-Type': 'application/json',
-          'Authorization':  localStorage.getItem('roma_acceso')
-        }
-      )
-    };
     return this.http.get(environment.apiEndpoint + '/getClientesBusqueda/' + texto_busqueda, httpOptions);
   }
 
   getClientesTodos(): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders(
-        { 'Content-Type': 'application/json',
-          'Authorization':  localStorage.getItem('roma_acceso')
-        }
-      )
-    };
     return this.http.get(environment.apiEndpoint + '/getClientesTodos/', httpOptions);
   }
 
   getDatosClientePorId(id) {
-    const httpOptions = {
-      headers: new HttpHeaders(
-        { 'Content-Type': 'application/json',
-          'Authorization':  localStorage.getItem('roma_acceso')
-        }
-      )
-    };
     return this.http.get(environment.apiEndpoint + '/getDatosClientePorId/' + id, httpOptions);
   }
 
+  getClientesWhere(campo_busqueda: string, texto_busqueda: string): Observable<any> {
+    return this.http.get(environment.apiEndpoint + '/getClientesWhere/' + campo_busqueda + '/' + texto_busqueda, httpOptions);
+  }
+
+  //PAGINACION INICIO --------->
+
+  getCantidadPaginasClientes(buscar_nombre, buscar_apellido, buscar_dni, buscar_fecha_nac, txt): Observable<any> {
+    return this.http.get(environment.apiEndpoint + '/getCantidadPaginasClientes/' + buscar_nombre + '/' + buscar_apellido + '/'  + buscar_dni + '/' + buscar_fecha_nac + '/' + txt, httpOptions);
+  }
+
+  getClientes(pagina_actual, cantidad_paginas, buscar_nombre, buscar_apellido, buscar_dni, buscar_fecha_nac, txt): Observable<any> {
+    return this.http.get(environment.apiEndpoint + '/getClientes/' + pagina_actual + '/' + cantidad_paginas + '/' + buscar_nombre + '/' + buscar_apellido + '/' + buscar_dni + '/' + buscar_fecha_nac + '/' + txt, httpOptions);
+  }
+  //PAGINACION FIN <------------
+
+
+  //---------------------------POST's---------------------------//
   insertClientePersonaDomicilio(empleado: any) {
-    const httpOptions = {
-      headers: new HttpHeaders(
-        { 'Content-Type': 'application/json',
-          'Authorization':  localStorage.getItem('roma_acceso')
-        }
-      )
-    };
     const json = JSON.stringify(empleado);
     return this.http.post(environment.apiEndpoint + '/insertClientePersonaDomicilio/', json, httpOptions);
   }
 
-  getClientesWhere(campo_busqueda: string, texto_busqueda: string): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders(
-        { 'Content-Type': 'application/json',
-          'Authorization':  localStorage.getItem('roma_acceso')
-        }
-      )
-    };
-    return this.http.get(environment.apiEndpoint + '/getClientesWhere/'+campo_busqueda+'/'+texto_busqueda, httpOptions);
-  }
 
+  //---------------------------PUT's---------------------------//
+
+  //---------------------------DELETE's---------------------------//
 
 
 
