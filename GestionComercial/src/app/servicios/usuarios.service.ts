4e0240dc601a5573a341fb8be56afc7ac99e3a07
erojas91 <esteban.j.rojas50@gmail.com>
2020-01-28 15:04:02 -0300

    Utilización de RouterLink en barra de navegación. Paginación de empleados y usuarios.

--- GestionComercial/src/app/servicios/usuarios.service.ts
+++ GestionComercial/src/app/servicios/usuarios.service.ts
@@ -19,6 +19,9 @@ export class UsuariosService {
 
   constructor(private http: HttpClient) { }
 
+
+  //GET's
+
   getDatosUsuario(usuario) {
     return this.http.get(environment.apiEndpoint + '/getDatosUsuario/' + usuario, httpOptions);
   }
@@ -31,10 +34,64 @@ export class UsuariosService {
     return this.http.get(environment.apiEndpoint + '/getUsuariosTodos/', httpOptions);
   }
 
+  getDatosUsuariosCargados(id_usuario: number): Observable<any> {
+    return this.http.get(environment.apiEndpoint + '/getDatosUsuariosCargados/' + id_usuario, httpOptions);
+  }
+
+  getPerfilesAsignados(id_perfiles: number): Observable<any> {
+    return this.http.get(environment.apiEndpoint + '/getPerfilesAsignados/' + id_perfiles, httpOptions);
+  }
+
+  getPerfilesSinAsignar(id_perfiles: number): Observable<any> {
+    return this.http.get(environment.apiEndpoint + '/getPerfilesSinAsignar/' + id_perfiles, httpOptions);
+  }
+
+  validarPassVieja(usuario, password) {
+    return this.http.get(environment.apiEndpoint + '/validarPassVieja/' + usuario + '/' + password, { observe: 'response' });
+  }
+
+  //PAGINACION INICIO --------->
+
+  getCantidadPaginasUsuarios(buscar_nombre, buscar_usuario, buscar_descripcion, txt): Observable<any> {
+    return this.http.get(environment.apiEndpoint +
+      '/getCantidadPaginasUsuarios/' +
+      buscar_nombre + '/' +
+      buscar_usuario + '/' +
+      buscar_descripcion + '/' +
+      txt,
+      httpOptions);
+  }
+
+  getUsuarios(pagina_actual, cantidad_paginas, buscar_nombre, buscar_usuario, buscar_descripcion, txt): Observable<any> {
+    return this.http.get(environment.apiEndpoint +
+      '/getUsuarios/' +
+      pagina_actual + '/' +
+      cantidad_paginas + '/' +
+      buscar_nombre + '/' +
+      buscar_usuario + '/' +
+      buscar_descripcion + '/' +
+      txt,
+      httpOptions);
+  }
+  //PAGINACION FIN <------------
+
+
+  //POST's
+
+  insertPerfilesAsignados(perfiles: any, id_usuario: number) {
+    const url = environment.apiEndpoint + `/insertPerfilesAsignados`;
+    let json = JSON.stringify({
+      "perfiles_id": perfiles.id
+      , "usuarios_id": id_usuario
+    });
+    console.log("Este es el json que armo en el service:" + json);
+    return this.http.post(url, json, httpOptions);
+  }
+
   insertUsuarioReturnId(datos: any, usuarios: any) {
     const url = environment.apiEndpoint + `/insertUsuarioReturnId`;
     let json = JSON.stringify({
-        "nombre_usuario": datos.nombre_usuario
+      "nombre_usuario": datos.nombre_usuario
       , "chk_debug": datos.chk_debug
       , "usuario": datos.nomb_usr
       , "personas_id": usuarios[0].personas_id
@@ -43,47 +100,26 @@ export class UsuariosService {
     return this.http.post(url, json, httpOptions);
   }
 
+
+
+  //PUT's
+
+  cambiarPassword(usuario: any, password: any): Observable<any> {
+    let json = JSON.stringify({ "usuario": usuario, "password": password });
+    console.log("Este es el json que armo en el service:" + json);
+    return this.http.put(environment.apiEndpoint + '/cambiarPassword', json, httpOptions);
+  }
+
   actualizarDatosUsuarios(datos: any): Observable<any> {
     let json = JSON.stringify(datos);
     return this.http.put(environment.apiEndpoint + '/actualizarDatosUsuarios', json, httpOptions);
   }
 
-  getDatosUsuariosCargados(id_usuario: number): Observable<any> {
-    return this.http.get(environment.apiEndpoint + '/getDatosUsuariosCargados/' + id_usuario, httpOptions);
-  }
+  //DELETE's
 
-  getPerfilesAsignados(id_perfiles: number): Observable<any> {
-    return this.http.get(environment.apiEndpoint + '/getPerfilesAsignados/' + id_perfiles, httpOptions);
-  }
-  
-  getPerfilesSinAsignar(id_perfiles: number): Observable<any> {
-    return this.http.get(environment.apiEndpoint + '/getPerfilesSinAsignar/' + id_perfiles, httpOptions);
-  }
-
-  deletePerfiles(id_usuario: number): Observable<any>{
+  deletePerfiles(id_usuario: number): Observable<any> {
     const url = environment.apiEndpoint + `/deletePerfiles/` + id_usuario;
     return this.http.delete(url, httpOptions);
-  }
-
-  insertPerfilesAsignados(perfiles: any, id_usuario: number) {
-    const url = environment.apiEndpoint + `/insertPerfilesAsignados`;
-    let json = JSON.stringify({
-        "perfiles_id": perfiles.id
-      , "usuarios_id": id_usuario
-    });
-    console.log("Este es el json que armo en el service:" + json);
-    return this.http.post(url, json, httpOptions);
-  }
-
-
-  validarPassVieja(usuario, password){
-    return this.http.get(environment.apiEndpoint+'/validarPassVieja/'+usuario+'/'+password, {observe:'response'});
-  }
-
-  cambiarPassword(usuario: any, password: any): Observable<any> {
-    let json = JSON.stringify({"usuario": usuario, "password": password});
-    console.log("Este es el json que armo en el service:" + json);
-    return this.http.put(environment.apiEndpoint + '/cambiarPassword', json, httpOptions);
   }
 
 
