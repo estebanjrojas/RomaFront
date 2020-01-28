7f68ab05f8b2ee8d9615f2c219cbad3942f4e088
erojas91 <esteban.j.rojas50@gmail.com>
2020-01-28 17:16:09 -0300

    Paginación Categorias

--- GestionComercial/src/app/servicios/categorias.service.ts
+++ GestionComercial/src/app/servicios/categorias.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Categorias } from '../../app/modelos/Categorias';
 import { Observable } from 'rxjs';
 
-
+const httpOptions = {
+  headers: new HttpHeaders(
+    {
+      'Content-Type': 'application/json',
+      'Authorization': localStorage.getItem('roma_acceso')
+    }
+  )
+};
 
 
 @Injectable({
@@ -19,48 +26,56 @@ export class CategoriasService {
   setCategorias(json) {
     const httpOptions = {
       headers: new HttpHeaders(
-        { 'Content-Type': 'application/json',
-          'Authorization':  localStorage.getItem('roma_acceso')
+        {
+          'Content-Type': 'application/json',
+          'Authorization': localStorage.getItem('roma_acceso')
         }
       )
     };
     this.categorias = json;
   }
 
-  obtenerJSONTodasCategorias(){
+
+  //GET's
+
+
+  obtenerJSONTodasCategorias() {
     const httpOptions = {
       headers: new HttpHeaders(
-        { 'Content-Type': 'application/json',
-          'Authorization':  localStorage.getItem('roma_acceso')
+        {
+          'Content-Type': 'application/json',
+          'Authorization': localStorage.getItem('roma_acceso')
         }
       )
     };
-    return this.http.get(environment.apiEndpoint+'/obtenerJSONTodasCategorias/', httpOptions);
+    return this.http.get(environment.apiEndpoint + '/obtenerJSONTodasCategorias/', httpOptions);
   }
 
   public getCategorias(): any {
     const httpOptions = {
       headers: new HttpHeaders(
-        { 'Content-Type': 'application/json',
-          'Authorization':  localStorage.getItem('roma_acceso')
+        {
+          'Content-Type': 'application/json',
+          'Authorization': localStorage.getItem('roma_acceso')
         }
       )
     };
-      const categoriasObservable = new Observable(observer => {
-            setTimeout(() => {
-                observer.next(this.categorias);
-            }, 1000);
-      });
+    const categoriasObservable = new Observable(observer => {
+      setTimeout(() => {
+        observer.next(this.categorias);
+      }, 1000);
+    });
 
-      return categoriasObservable;
+    return categoriasObservable;
   }
 
 
   getCategoriasBusqueda(texto_busqueda): Observable<any> {
     const httpOptions = {
       headers: new HttpHeaders(
-        { 'Content-Type': 'application/json',
-          'Authorization':  localStorage.getItem('roma_acceso')
+        {
+          'Content-Type': 'application/json',
+          'Authorization': localStorage.getItem('roma_acceso')
         }
       )
     };
@@ -70,39 +85,74 @@ export class CategoriasService {
   getCategoriasTodas(): Observable<any> {
     const httpOptions = {
       headers: new HttpHeaders(
-        { 'Content-Type': 'application/json',
-          'Authorization':  localStorage.getItem('roma_acceso')
+        {
+          'Content-Type': 'application/json',
+          'Authorization': localStorage.getItem('roma_acceso')
         }
       )
     };
     return this.http.get(environment.apiEndpoint + '/getCategoriasTodas/', httpOptions);
   }
 
+  //PAGINACION INICIO --------->
+
+  getCantidadPaginasCategorias(buscar_nombre, buscar_descripcion, buscar_catpadre, txt): Observable<any> {
+    return this.http.get(environment.apiEndpoint +
+      '/getCantidadPaginasCategorias/' +
+      buscar_nombre + '/' +
+      buscar_descripcion + '/' +
+      buscar_catpadre + '/' +
+      txt,
+      httpOptions);
+  }
+
+  getCategoria(pagina_actual, cantidad_paginas, buscar_nombre, buscar_descripcion, buscar_catpadre, txt): Observable<any> {
+    return this.http.get(environment.apiEndpoint +
+      '/getCategorias/' +
+      pagina_actual + '/' +
+      cantidad_paginas + '/' +
+      buscar_nombre + '/' +
+      buscar_descripcion + '/' +
+      buscar_catpadre + '/' +
+      txt,
+      httpOptions);
+  }
+  //PAGINACION FIN <------------
+
+
+  //POST's
 
   guardarCategoria(categoria: any) {
     const httpOptions = {
       headers: new HttpHeaders(
-        { 'Content-Type': 'application/json',
-          'Authorization':  localStorage.getItem('roma_acceso')
+        {
+          'Content-Type': 'application/json',
+          'Authorization': localStorage.getItem('roma_acceso')
         }
       )
     };
     const json = JSON.stringify(categoria);
-    return this.http.post(environment.apiEndpoint+'/categorias/insert', json, httpOptions);
+    return this.http.post(environment.apiEndpoint + '/categorias/insert', json, httpOptions);
   }
 
+
+  //PUT's
 
   updateCategoria(categoria: any) {
     const httpOptions = {
       headers: new HttpHeaders(
-        { 'Content-Type': 'application/json',
-          'Authorization':  localStorage.getItem('roma_acceso')
+        {
+          'Content-Type': 'application/json',
+          'Authorization': localStorage.getItem('roma_acceso')
         }
       )
     };
     const json = JSON.stringify(categoria);
-    return this.http.post(environment.apiEndpoint+'/categorias/update', json, httpOptions);
+    return this.http.post(environment.apiEndpoint + '/categorias/update', json, httpOptions);
   }
+
+
+  //DELETE's
 
 
 
