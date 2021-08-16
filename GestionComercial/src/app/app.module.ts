ec208276ff50d3e56305b85c8580b1d507573e7e
raul91 <raul.martinez230@gmail.com>
2021-08-16 16:35:50 -0300

    Paso los componentes relacionados a empleados, clientes, puntos de venta y usuarios al módulo Herramientas Administrativas

--- GestionComercial/src/app/app.module.ts
+++ GestionComercial/src/app/app.module.ts
@@ -18,20 +18,12 @@ import { CabeceraComponent } from './componentes/interfaz/cabecera/cabecera.comp
 import { PieComponent } from './componentes/interfaz/pie/pie.component';
 import { NavegacionComponent } from './componentes/interfaz/navegacion/navegacion.component';
 import { LoginComponent } from './componentes/login/login.component';
-import { CargarEmpleadosComponent } from './componentes/adm-empleados/cargar-empleados/cargar-empleados.component';
-import { CargarPuntosDeVentaComponent } from './componentes/adm-puntos-de-venta/cargar-puntos-de-venta/cargar-puntos-de-venta.component';
-import { BuscarPuntosDeVentaComponent } from './componentes/adm-puntos-de-venta/buscar-puntos-de-venta/buscar-puntos-de-venta.component';
-import { CargarUsuariosComponent } from './componentes/adm-usuarios/cargar-usuarios/cargar-usuarios.component';
-import { BuscarUsuariosComponent } from './componentes/adm-usuarios/buscar-usuarios/buscar-usuarios.component';
 import { CargarProductosComponent } from './componentes/adm-productos/cargar-productos/cargar-productos.component';
 import { BuscarProductosComponent } from './componentes/adm-productos/buscar-productos/buscar-productos.component';
-import { AdministrarPerfilesComponent } from './componentes/adm-usuarios/administrar-perfiles/administrar-perfiles.component';
 import { CargarCategoriaComponent } from './componentes/adm-categoria/cargar-categoria/cargar-categoria.component';
 import { BuscarCategoriaComponent } from './componentes/adm-categoria/buscar-categoria/buscar-categoria.component';
 import { CargarPromocionesComponent } from './componentes/adm-promociones/cargar-promociones/cargar-promociones.component';
 import { BuscarPromocionesComponent } from './componentes/adm-promociones/buscar-promociones/buscar-promociones.component';
-import { BuscarClientesComponent } from './componentes/adm-clientes/buscar-clientes/buscar-clientes.component';
-import { CargarClientesComponent, MomentUtcDateAdapter } from './componentes/adm-clientes/cargar-clientes/cargar-clientes.component';
 import { DetalleProductoComponent } from './componentes/adm-productos/detalle-producto/detalle-producto.component';
 import { BusquedaVentasComponent } from './componentes/ventas/busqueda-ventas/busqueda-ventas.component';
 import { NuevaVentaComponent } from './componentes/ventas/nueva-venta/nueva-venta.component';
@@ -52,32 +44,19 @@ import { HerramientasAdministrativasModule } from './herramientas-administrativa
 const appRoutes: Routes = [{ path: '', component: LoginComponent }
   , { path: 'login', component: LoginComponent }
   , { path: 'home', component: HomeComponent }
-  , { path: 'empleados/cargar-empleados', component: CargarEmpleadosComponent }
-  , { path: 'empleados/cargar-empleados/:empleados_id', component: CargarEmpleadosComponent }  
   , { path: 'productos/cargar-productos', component: CargarProductosComponent }
   , { path: 'productos/cargar-productos/:productos_id', component: CargarProductosComponent }
   , { path: 'productos/busqueda-productos', component: BuscarProductosComponent }
-  , { path: 'puntos-venta/cargar-puntos-venta', component: CargarPuntosDeVentaComponent }
-  , { path: 'puntos-venta/cargar-puntos-venta/:puntos_venta_id', component: CargarPuntosDeVentaComponent }
-  , { path: 'puntos-venta/busqueda-puntos-venta', component: BuscarPuntosDeVentaComponent }
-  , { path: 'usuarios/cargar-usuarios', component: CargarUsuariosComponent }
-  , { path: 'usuarios/cargar-usuarios/:usuarios_id', component: CargarUsuariosComponent }
-  , { path: 'usuarios/busqueda-usuarios', component: BuscarUsuariosComponent }
-  , { path: 'usuarios/administrar-perfiles', component: AdministrarPerfilesComponent }
-  , { path: 'usuarios/administrar-perfiles/:empleados_id', component: AdministrarPerfilesComponent }
   , { path: 'usuarios/cambiar-password', component: CambiarPasswordComponent }
   , { path: 'categorias/cargar-categorias', component: CargarCategoriaComponent }
   , { path: 'categorias/cargar-categorias/:categorias_id', component: CargarCategoriaComponent }
   , { path: 'categorias/busqueda-categorias', component: BuscarCategoriaComponent }
   , { path: 'promociones/cargar-promociones', component: CargarPromocionesComponent }
   , { path: 'promociones/busqueda-promociones', component: BuscarPromocionesComponent }
-  , { path: 'clientes/cargar-clientes', component: CargarClientesComponent }
-  , { path: 'clientes/cargar-clientes/:clientes_id', component: CargarClientesComponent }
-  , { path: 'clientes/busqueda-clientes', component: BuscarClientesComponent }
   , { path: 'clientes/detalle-producto', component: DetalleProductoComponent }
   , { path: 'ventas/busqueda-ventas', component: BusquedaVentasComponent }
   , { path: 'ventas/nueva-venta', component: NuevaVentaComponent }
-  //, { path: 'admin', loadChildren: ()=>import('./herramientas-administrativas/herramientas-administrativas.module').then(m => m.HerramientasAdministrativasModule)}
+  
 ];
 
 
@@ -88,20 +67,12 @@ const appRoutes: Routes = [{ path: '', component: LoginComponent }
     PieComponent,
     NavegacionComponent,
     LoginComponent,
-    CargarEmpleadosComponent,
-    BuscarPuntosDeVentaComponent,
-    CargarPuntosDeVentaComponent,
-    BuscarUsuariosComponent,
-    CargarUsuariosComponent,
-    AdministrarPerfilesComponent,
     BuscarProductosComponent,
     CargarProductosComponent,
     BuscarCategoriaComponent,
     CargarCategoriaComponent,
     BuscarPromocionesComponent,
     CargarPromocionesComponent,
-    BuscarClientesComponent,
-    CargarClientesComponent,
     DetalleProductoComponent,
     BusquedaVentasComponent,
     NuevaVentaComponent,
@@ -147,7 +118,6 @@ const appRoutes: Routes = [{ path: '', component: LoginComponent }
   providers: [{provide: APP_BASE_HREF, useValue: ''},
   { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
   { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
-  { provide: DateAdapter, useClass: MomentUtcDateAdapter },
   {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 2500}}],
   
   bootstrap: [AppComponent]
--- GestionComercial/src/app/herramientas-administrativas/herramientas-administrativas.module.ts
+++ GestionComercial/src/app/herramientas-administrativas/herramientas-administrativas.module.ts
@@ -2,19 +2,79 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { BusquedaEmpleadosComponent } from '../componentes/adm-empleados/busqueda-empleados/busqueda-empleados.component';
+import {
+  MatButtonModule, MatCheckboxModule, MatFormFieldModule,
+  MatAutocompleteModule, MatOptionModule, MatInputModule,
+  MatDatepickerModule, MatNativeDateModule, MatSelectModule,
+  MatTreeModule, MatIconModule, MatRadioModule, MatCardModule,
+  MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS
+} from '@angular/material';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material';
+import { MAT_MOMENT_DATE_FORMATS } from '@angular/material-moment-adapter';
 
-const appRoutes: Routes = [{ path: 'empleados/busqueda-empleados', component: BusquedaEmpleadosComponent }]
+import { BusquedaEmpleadosComponent } from '../componentes/adm-empleados/busqueda-empleados/busqueda-empleados.component';
+import { CargarEmpleadosComponent } from '../componentes/adm-empleados/cargar-empleados/cargar-empleados.component';
+import { CargarPuntosDeVentaComponent } from '../componentes/adm-puntos-de-venta/cargar-puntos-de-venta/cargar-puntos-de-venta.component';
+import { BuscarPuntosDeVentaComponent } from '../componentes/adm-puntos-de-venta/buscar-puntos-de-venta/buscar-puntos-de-venta.component';
+import { CargarUsuariosComponent } from '../componentes/adm-usuarios/cargar-usuarios/cargar-usuarios.component';
+import { BuscarUsuariosComponent } from '../componentes/adm-usuarios/buscar-usuarios/buscar-usuarios.component';
+import { AdministrarPerfilesComponent } from '../componentes/adm-usuarios/administrar-perfiles/administrar-perfiles.component';
+import { BuscarClientesComponent } from '../componentes/adm-clientes/buscar-clientes/buscar-clientes.component';
+import { CargarClientesComponent, MomentUtcDateAdapter } from '../componentes/adm-clientes/cargar-clientes/cargar-clientes.component';
+
+
+const appRoutes: Routes = [{ path: 'empleados/busqueda-empleados', component: BusquedaEmpleadosComponent }
+, { path: 'empleados/cargar-empleados', component: CargarEmpleadosComponent }
+, { path: 'empleados/cargar-empleados/:empleados_id', component: CargarEmpleadosComponent }
+, { path: 'puntos-venta/cargar-puntos-venta', component: CargarPuntosDeVentaComponent }
+, { path: 'puntos-venta/cargar-puntos-venta/:puntos_venta_id', component: CargarPuntosDeVentaComponent }
+, { path: 'puntos-venta/busqueda-puntos-venta', component: BuscarPuntosDeVentaComponent }
+, { path: 'usuarios/cargar-usuarios', component: CargarUsuariosComponent }
+, { path: 'usuarios/cargar-usuarios/:usuarios_id', component: CargarUsuariosComponent }
+, { path: 'usuarios/busqueda-usuarios', component: BuscarUsuariosComponent }
+, { path: 'usuarios/administrar-perfiles', component: AdministrarPerfilesComponent }
+, { path: 'usuarios/administrar-perfiles/:empleados_id', component: AdministrarPerfilesComponent }
+, { path: 'clientes/cargar-clientes', component: CargarClientesComponent }
+, { path: 'clientes/cargar-clientes/:clientes_id', component: CargarClientesComponent }
+, { path: 'clientes/busqueda-clientes', component: BuscarClientesComponent }]
 
 @NgModule({
   declarations: [
-    BusquedaEmpleadosComponent
+    BusquedaEmpleadosComponent,
+    CargarEmpleadosComponent,
+    CargarPuntosDeVentaComponent,
+    BuscarPuntosDeVentaComponent,
+    CargarUsuariosComponent,
+    BuscarUsuariosComponent,
+    AdministrarPerfilesComponent,
+    BuscarClientesComponent,
+    CargarClientesComponent,
   ],
   imports: [
     CommonModule,
     RouterModule.forRoot(appRoutes),
     FormsModule,
     ReactiveFormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatOptionModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatTreeModule,
+    MatIconModule,
+    MatRadioModule,
+    MatCardModule,
+    MatSnackBarModule,
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
+    { provide: DateAdapter, useClass: MomentUtcDateAdapter },
+    {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 2500}}
   ]
 })
 export class HerramientasAdministrativasModule { }
