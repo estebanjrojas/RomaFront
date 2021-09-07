8015e1aa0c7498048dfaf8cb87fe5319cf1cb8a5
raul91 <raul.martinez230@gmail.com>
2021-09-07 13:28:33 -0300

    Borrador para Prototipo de Home Responsive y Correcciòn de Barra de Navegaciòn Dinámica.

--- GestionComercial/src/app/componentes/interfaz/navegacion-submenu/navegacion-submenu.component.ts
+++ GestionComercial/src/app/componentes/interfaz/navegacion-submenu/navegacion-submenu.component.ts
@@ -11,6 +11,26 @@ export class NavegacionSubmenuComponent implements OnInit {
   }
   
   ngOnInit() {
+    console.log(this.ObjeMenu);
+    console.log(this.opcionMenuTieneHijos(this.ObjeMenu));
+  }
+
+  
+  opcionMenuTieneRuta(menuOpcion){
+    return (menuOpcion.menu_ruta===null 
+      || menuOpcion.menu_ruta === undefined
+      || menuOpcion.menu_ruta === "" 
+      || menuOpcion.menu_ruta === "null"
+      ) ? false : true;
+  }
+
+  opcionMenuTieneHijos(menuOpcion){
+    return (menuOpcion.menu_hijo != null
+      && menuOpcion.menu_hijo != undefined
+      && menuOpcion.menu_hijo != "null"
+      && menuOpcion.menu_hijo != ""
+      && menuOpcion.menu_hijo.length > 0
+      ) ? true : false;
   }
 
 }
--- GestionComercial/src/app/componentes/interfaz/navegacion/navegacion.component.ts
+++ GestionComercial/src/app/componentes/interfaz/navegacion/navegacion.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsuariosService} from '../../../servicios/usuarios.service';
 import { AuthService } from '../../../servicios/auth.service';
+import { isArray } from 'util';
 
 @Component({
   selector: 'app-navegacion',
@@ -19,11 +20,17 @@ export class NavegacionComponent implements OnInit {
     let usuario = this.Auth.getNombreUsuarioSesion();
     let menuLS = this.Auth.getMenuUsuarioSesion();
     this.menuArray = JSON.parse(menuLS).menu;
+    /*
+    this.menuArray = this.menuArray.map(menu => {
+      return {...menu, tiene_ruta: this.opcionMenuTieneRuta(menu), tiene_hijos: this.opcionMenuTieneHijos(menu)};
+    })*/
+
+    console.log(this.menuArray);
 
     this.SrvUsuarios.getDatosUsuario(usuario).subscribe(respuesta=>{
       let cast = respuesta;
       this.datos_usuario = cast[0];
-      console.log(this.datos_usuario);
+     // console.log(this.datos_usuario);
       this.Auth.setDebug(this.datos_usuario.debug);
       
       if(this.Auth.getDebugUsuarioSesion()==1) {
@@ -32,6 +39,24 @@ export class NavegacionComponent implements OnInit {
     });
 
   }
+
+   opcionMenuTieneRuta(menuOpcion){
+    return (menuOpcion.menu_ruta===null 
+      || menuOpcion.menu_ruta === undefined
+      || menuOpcion.menu_ruta === "" 
+      || menuOpcion.menu_ruta === "null"
+      ) ? false : true;
+  }
+
+  opcionMenuTieneHijos(menuOpcion){
+    return (menuOpcion.menu_hijo != null
+      && menuOpcion.menu_hijo != undefined
+      && menuOpcion.menu_hijo != "null"
+      && menuOpcion.menu_hijo != ""
+      && menuOpcion.menu_hijo.length > 0
+      ) ? true : false;
+  }
+
 
   cerrarSession(){
     localStorage.clear();
