07394a6eb8dcead661ac0a68e4eac45db5e47e3c
raul91 <raul.martinez230@gmail.com>
2022-03-12 17:21:36 -0300

    Corrijo error en import. Update Angular 9 completo

--- GestionComercial/src/app/comunes/interfaces/Domicilios.ts
+++ GestionComercial/src/app/comunes/interfaces/Domicilios.ts
@@ -1,14 +1,14 @@
-import { Ciudades} from './ciudades';
+import { Ciudades } from "./Ciudades";
 
-export class Domicilios{
-    domicilios_id : number;
-    calle ?: string;
-    numero ?: string;
-    piso ?: string;
-    depto ?: string;
-    manzana ?: string;
-    lote ?: string;
-    block ?: string;
-    barrio ?: string;
-    ciudad ?: Ciudades;
+export class Domicilios {
+  domicilios_id: number;
+  calle?: string;
+  numero?: string;
+  piso?: string;
+  depto?: string;
+  manzana?: string;
+  lote?: string;
+  block?: string;
+  barrio?: string;
+  ciudad?: Ciudades;
 }
